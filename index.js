6db5bc56b37f9441c2d7f0e3b227d3fd6be03ecc
rayyankhan00776 <rkkhan0750@gmail.com>
2026-05-17 23:08:46 +0500

    updated the ci

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const PORT =  3000;
+const PORT =  1200;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log('server successfully running!');
